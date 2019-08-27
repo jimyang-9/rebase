1f571517c4014d8216799a71e925eaf5999cccaa
Jim Yang <jim.yang@onepeloton.com>
2019-08-27 16:30:52 -0400

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,7 @@ if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
 	exit 1
 fi
 
+git init
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
