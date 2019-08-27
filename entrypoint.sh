21df14c10aaa47cb3f82b6b132a0a80e204c6849
Jim Yang <jim.yang@onepeloton.com>
2019-08-27 16:37:08 -0400

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
 fi
 
 git init
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
