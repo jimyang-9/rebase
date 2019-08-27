20c6425354bd0f08092b8ea60f6c55c7f72b998c
Jim Yang <jim.yang@onepeloton.com>
2019-08-27 16:26:20 -0400

    pulls

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,14 +24,14 @@ API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
 pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/pull/$PR_NUMBER")
+          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
 
 BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
 BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
 
 echo "shmee response: $pr_resp"
 
-if [ -z "$BASE_BRANCH" ] || [$BASE_BRANCH = "null" ]; then
+if [ -z "$BASE_BRANCH" ] || ["$BASE_BRANCH" = "null" ]; then
 	echo "Cannot get base branch information for PR #$PR_NUMBER!"
 	echo "API response: $pr_resp"
 	exit 1
